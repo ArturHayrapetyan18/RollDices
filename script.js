4913a5df37e51e87a8173210482ad609fbc005ac
davitsargsyan92 <sargsyandavit1992@gmail.com>
2021-09-11 10:33:22 +0400

    project setup

---
+++ script.js
@@ -0,0 +1,34 @@
+let images = [
+    "./images/dice-01.svg",
+    "./images/dice-02.svg",
+    "./images/dice-06.svg",
+    "./images/dice-04.svg",
+    "./images/dice-05.svg"
+]
+const rollButton = document.getElementById("roll")
+let dice = document.querySelectorAll("img");
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake")
+        
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        })
+        let dieOneValue = Math.floor(Math.random()*images.length);
+        let dieTwoValue = Math.floor(Math.random()*images.length);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue])
+document.querySelector("#die-2").setAttribute("src", images[dieTwoValue])
+
+document.querySelector("#total").innerHTML = "Your roll is " + ( ((dieOneValue)+1) + ((dieTwoValue)+1) );
+
+console.log(dieOneValue, dieTwoValue)
+    },
+    1000);
+}
+
+
+
+roll()
+rollButton.addEventListener("click", roll)
